689eedc1e343b01b95e8b9820fd9072133dd545b
Ronald Casili <nvlled@gmail.com>
2025-08-11 12:50:23 +0800

    Replace EnumSet with IntegerBitSet and use partial enum matching

--- src/root.zig
+++ src/root.zig
@@ -1,35 +1,6 @@
 const std = @import("std");
+const BitSet = std.bit_set.IntegerBitSet(256);
 const Allocator = std.mem.Allocator;
-
-const U8Enum = enum(u8) {
-
-    // TODO: better way of doing this?
-    // zig fmt: off
-    _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18,
-    _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35,
-    _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52,
-    _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64, _65, _66, _67, _68, _69,
-    _70, _71, _72, _73, _74, _75, _76, _77, _78, _79, _80, _81, _82, _83, _84, _85, _86,
-    _87, _88, _89, _90, _91, _92, _93, _94, _95, _96, _97, _98, _99, _100, _101, _102,
-    _103, _104, _105, _106, _107, _108, _109, _110, _111, _112, _113, _114, _115, _116,
-    _117, _118, _119, _120, _121, _122, _123, _124, _125, _126, _127, _128, _129, _130,
-    _131, _132, _133, _134, _135, _136, _137, _138, _139, _140, _141, _142, _143, _144,
-    _145, _146, _147, _148, _149, _150, _151, _152, _153, _154, _155, _156, _157, _158,
-    _159, _160, _161, _162, _163, _164, _165, _166, _167, _168, _169, _170, _171, _172,
-    _173, _174, _175, _176, _177, _178, _179, _180, _181, _182, _183, _184, _185, _186,
-    _187, _188, _189, _190, _191, _192, _193, _194, _195, _196, _197, _198, _199, _200,
-    _201, _202, _203, _204, _205, _206, _207, _208, _209, _210, _211, _212, _213, _214,
-    _215, _216, _217, _218, _219, _220, _221, _222, _223, _224, _225, _226, _227, _228,
-    _229, _230, _231, _232, _233, _234, _235, _236, _237, _238, _239, _240, _241, _242,
-    _243, _244, _245, _246, _247, _248, _249, _250, _251, _252, _253, _254,
-    // zig fmt: on
-
-    fn enumSet(str: []const u8) std.EnumSet(@This()) {
-        var set: std.EnumSet(U8Enum) = .initEmpty();
-        for (str) |c| set.insert(@enumFromInt(c));
-        return set;
-    }
-};
 
 const Regex = union(enum) {
     const Self = @This();
@@ -70,7 +41,7 @@ const Regex = union(enum) {
     @"!alphanum",
     @"!whitespace",
 
-    _charset: std.EnumSet(U8Enum),
+    _charset: BitSet,
 
     pub const Match = struct {
         pos: usize,
@@ -109,12 +80,6 @@ const Regex = union(enum) {
                 return self.value[self.pos];
             return null;
         }
-        fn next(self: @This()) ?u8 {
-            const len = self.value.len;
-            if (self.pos < len - 1 and self.value.len > 0)
-                return self.value[self.pos + 1];
-            return null;
-        }
 
         fn slice(self: @This(), offset: usize) Input {
             return .{
@@ -125,38 +90,18 @@ const Regex = union(enum) {
     };
 
     const CharClass = struct {
-        const digit = U8Enum.enumSet("0123456789");
-        const alphabet = U8Enum.enumSet("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        const underscore = U8Enum.enumSet("_");
-        const whitespace = U8Enum.enumSet(" \t\n\r\x0C\x0B\x85");
-        const any = U8Enum.enumSet("").complement();
+        const any = BitSet.initFull();
+        const digit = fromString("0123456789");
+        const alphabet = fromString("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        const underscore = fromString("_");
+        const whitespace = fromString(" \t\n\r\x0C\x0B\x85");
         const alphanum = digit.unionWith(alphabet);
         const word = alphanum.unionWith(underscore);
 
-        const not_word = word.complement();
-        const not_digit = digit.complement();
-        const not_alphabet = alphabet.complement();
-        const not_alphanum = alphanum.complement();
-        const not_whitespace = whitespace.complement();
-
-        inline fn isWord(ch: u8) bool {
-            return word.contains(@enumFromInt(ch));
-        }
-
-        fn get(re: Regex) ?std.EnumSet(U8Enum) {
-            return switch (re) {
-                .word => word,
-                .digit => digit,
-                .alphabet => alphabet,
-                .alphanum => alphanum,
-                .whitespace => whitespace,
-                .@"!word" => not_word,
-                .@"!digit" => not_digit,
-                .@"!alphabet" => not_alphabet,
-                .@"!alphanum" => not_alphanum,
-                .@"!whitespace" => not_whitespace,
-                else => null,
-            };
+        fn fromString(str: []const u8) BitSet {
+            var set: BitSet = .initEmpty();
+            for (str) |c| set.set(c);
+            return set;
         }
     };
 
@@ -307,48 +252,28 @@ const Regex = union(enum) {
         return null;
     }
 
-    fn match(self: @This(), allocator: Allocator, input_arg: Input, state: *SearchState) Allocator.Error!?Match {
+    fn match(self: Self, allocator: Allocator, input_arg: Input, state: *SearchState) Allocator.Error!?Match {
         const match_one: Match = .{ .pos = 0, .len = 1 };
         const match_zero: Match = .{ .pos = 0, .len = 0 };
 
-        switch (self) {
-            .boundary, .start, .end => {},
-            else => {
-                const input = input_arg.string();
-                if (input.len == 0) return null;
-                if (CharClass.get(self)) |set| {
-                    const elem: U8Enum = @enumFromInt(input[0]);
-                    return if (set.contains(elem)) match_one else null;
-                }
-            },
-        }
-
         return switch (self) {
-            .any => match_one,
-
-            ._charset => |set| {
-                const input = input_arg.string();
-                const elem: U8Enum = @enumFromInt(input[0]);
-                return if (set.contains(elem)) match_one else null;
-            },
-
-            .start => if (input_arg.pos == 0) match_zero else null,
-            .end => {
-                const len = input_arg.size();
-                return if (len == 0 or input_arg.pos == len - 1) match_zero else null;
-            },
-
             .boundary => {
                 const prev = input_arg.prev() orelse return match_zero;
                 const next = input_arg.current() orelse return match_zero;
 
-                const is_prev_word = CharClass.isWord(prev);
-                const is_next_word = CharClass.isWord(next);
+                const is_prev_word = CharClass.word.isSet(prev);
+                const is_next_word = CharClass.word.isSet(next);
                 const matched =
                     is_prev_word and !is_next_word or
                     !is_prev_word and is_next_word;
 
                 return if (matched) match_zero else null;
+            },
+
+            .start => return if (input_arg.pos == 0) match_zero else null,
+            .end => {
+                const len = input_arg.size();
+                return if (len == 0 or input_arg.pos == len - 1) match_zero else null;
             },
 
             ._literal => |val| {
@@ -374,9 +299,9 @@ const Regex = union(enum) {
 
                 if (index >= 0 and index < state.captures.items.len) {
                     const input = input_arg.string();
-                    const val = state.captures.items[index];
-                    if (val.len > 0 and std.mem.eql(u8, val, input[0..@min(val.len, input.len)]))
-                        return .{ .pos = 0, .len = val.len };
+                    const item = state.captures.items[index];
+                    if (item.len > 0 and std.mem.eql(u8, item, input[0..@min(item.len, input.len)]))
+                        return .{ .pos = 0, .len = item.len };
                 }
                 return null;
             },
@@ -393,6 +318,43 @@ const Regex = union(enum) {
             ._repetition => handleConcat(allocator, input_arg, state, &.{&self}),
 
             ._concat => |args| handleConcat(allocator, input_arg, state, args),
+
+            inline .any,
+            .word,
+            .digit,
+            .alphabet,
+            .alphanum,
+            .whitespace,
+            .@"!word",
+            .@"!digit",
+            .@"!alphabet",
+            .@"!alphanum",
+            .@"!whitespace",
+            ._charset,
+            => |val, tag| {
+                if (input_arg.size() == 0) return null;
+
+                const input = input_arg.string();
+                const ch = input[0];
+                return switch (tag) {
+                    .any => match_one,
+
+                    .word => if (CharClass.word.isSet(ch)) match_one else null,
+                    .digit => if (CharClass.digit.isSet(ch)) match_one else null,
+                    .alphabet => if (CharClass.alphabet.isSet(ch)) match_one else null,
+                    .alphanum => if (CharClass.alphanum.isSet(ch)) match_one else null,
+                    .whitespace => if (CharClass.whitespace.isSet(ch)) match_one else null,
+
+                    .@"!word" => if (!CharClass.word.isSet(ch)) match_one else null,
+                    .@"!digit" => if (!CharClass.digit.isSet(ch)) match_one else null,
+                    .@"!alphabet" => if (!CharClass.alphabet.isSet(ch)) match_one else null,
+                    .@"!alphanum" => if (!CharClass.alphanum.isSet(ch)) match_one else null,
+                    .@"!whitespace" => if (!CharClass.whitespace.isSet(ch)) match_one else null,
+                    ._charset => if (val.isSet(ch)) match_one else null,
+
+                    else => comptime unreachable,
+                };
+            },
 
             else => {
                 std.debug.print("TODO: {s}\n", .{@tagName(self)});
@@ -423,24 +385,24 @@ const Regex = union(enum) {
     }
 
     fn anyChar(value: []const u8) Self {
-        var set: std.EnumSet(U8Enum) = .initEmpty();
-        for (value) |c| set.insert(@enumFromInt(c));
+        var set: BitSet = .initEmpty();
+        for (value) |c| set.set(c);
         return .{ ._charset = set };
     }
     fn @"!anyChar"(value: []const u8) Self {
-        var set: std.EnumSet(U8Enum) = .initEmpty();
-        for (value) |c| set.insert(@enumFromInt(c));
+        var set: BitSet = .initEmpty();
+        for (value) |c| set.set(c);
         return .{ ._charset = set.complement() };
     }
 
     fn range(start: u8, end: u8) Self {
-        var set: std.EnumSet(U8Enum) = .initEmpty();
-        for (start..end) |c| set.insert(@enumFromInt(c));
+        var set: BitSet = .initEmpty();
+        for (start..end) |c| set.set(c);
         return .{ ._charset = set };
     }
     fn @"!range"(start: u8, end: u8) Self {
-        var set: std.EnumSet(U8Enum) = .initEmpty();
-        for (start..end) |c| set.insert(@enumFromInt(c));
+        var set: BitSet = .initEmpty();
+        for (start..end) |c| set.set(c);
         return .{ ._charset = set.complement() };
     }
 
